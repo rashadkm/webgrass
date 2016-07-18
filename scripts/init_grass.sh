ff71ebfe3643296c31ff620a2bf6650c28215b23
Mayank <mayankagrawal333@gmail.com>
2016-07-18 15:21:59 +0530

    scripts

---
+++ scripts/init_grass.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GRASS_PNG_AUTO_WRITE=TRUE; 
+export GRASS_PNG_COMPRESSION=9; 
+export GRASS_TRANSPARENT=TRUE; 
+export GRASS_TRUECOLOR=TRUE; 
+export LD_LIBRARY_PATH=/usr/lib/grass70/lib; 
+export GISBASE=/usr/lib/grass70/; 
+export GISDBASE=/home/mayank/grassdata1; 
+export GISRC=/home/mayank/.grass7/rc; 
+export PATH=/usr/lib/grass70/bin:/usr/lib/grass70/scripts:$PATH;
