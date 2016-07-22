9d2ee5e61931c0683ad7020ea58ffbe0bb8d09a6
Mayank <mayankagrawal333@gmail.com>
2016-07-22 15:44:11 +0530

    test

--- src/MainUI.cpp
+++ src/MainUI.cpp
@@ -172,12 +172,7 @@ void MainUI::runModule(Wt::WDialog::DialogCode code){
 
                  }
         std::cout<<command<<std::endl;
-            // redi::ipstream proc("grass70 $HOME/grassdata/newLocation/PERMANENT", redi::pstreams::pstdout | redi::pstreams::pstdin );
-            //   std::string line;
-            //   std::cout.flush();
-            //   while (std::getline(proc.out(), line)){
-            //     std::cout << "stdout: " << line << '\n';
-            // }
+          
             redi::ipstream proc("g.gisenv");
             // redi::ipstream proc("export GRASS_PNG_AUTO_WRITE=TRUE; export GRASS_PNG_COMPRESSION=9; export GRASS_TRANSPARENT=TRUE; export GRASS_TRUECOLOR=TRUE; export LD_LIBRARY_PATH=/usr/lib/grass70/lib; export GISBASE=/usr/lib/grass70/; export GISDBASE=/home/mayank/grassdata1; export GISRC=/home/mayank/.grass7/rc; export PATH=/usr/lib/grass70/bin:/usr/lib/grass70/scripts:$PATH; g.guienv");
             // redi::ipstream proc("g.guienv");      
