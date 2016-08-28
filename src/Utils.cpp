1f536a4a38d9ddadec0fb544a630462971c11f53
Rashad Kanavath <mohammedrashadkm@gmail.com>
2016-08-28 13:46:57 +0200

    move parsing node value and attribute to static methods

---
+++ src/Utils.cpp
@@ -0,0 +1,71 @@
+#include <fstream>
+
+#include "Utils.h"
+#include <boost/algorithm/string.hpp>
+
+namespace Utils {
+
+  const std::string moduleDocRoot()
+  {
+    return Wt::WApplication::instance()->docRoot() + "/xml/";
+  }
+
+  const std::string checkFileExists(const std::string& fname)
+    {
+      std::ifstream stream(fname);     
+      return std::string(std::istreambuf_iterator<char>(stream), {});
+    }
+
+  void getValueOfAttribute(const XmlNodeType &node, const char* attr, std::string& value)
+  {
+  if( node != NULL && node.attribute(attr) != NULL )
+      value = node.attribute(attr).value();
+    else
+      {
+	//TODO: use logger
+#if defined(DEBUG_TRACE)
+	std::cerr << "node != NULL && node.attribute('" << attr <<"') != NULL false" << std::endl;
+#endif	
+      }
+    return;    
+  }
+
+  void getValueOfAttribute(const XmlNodeType &node, const std::string attr, std::string& value)
+  {
+    getValueOfAttribute( node, attr.c_str(), value );
+    return;
+  
+  }
+
+  void getValueOfChildNode(const XmlNodeType &node, const char* child, std::string& value)
+    
+  {
+    if( node != NULL && node.child(child) != NULL )
+      {
+	value = node.child(child).child_value();
+	boost::algorithm::trim(value);
+	//	boost::algorithm::trim_right(value);
+	//	boost::algorithm::trim_left(value);
+      }
+    else
+      {
+	//TODO: use logger
+#if defined(DEBUG_TRACE)	
+	std::cerr << "node != NULL && node.child('" << child <<"') != NULL false" << std::endl;
+#endif	
+      }
+    return;
+  }
+    
+  void getValueOfChildNode(const XmlNodeType &node, const std::string child, std::string& value)
+  {
+    getValueOfChildNode( node, child.c_str(), value );
+    return;
+  }
+
+  void alert(const std::string msg)
+  {
+    Wt::WApplication::instance()->doJavaScript( "alert('" + msg  + "');" );
+  }
+
+}
---
+++ src/Utils.h
@@ -0,0 +1,26 @@
+#ifndef UTILS_H_DEFINED
+#define UTILS_H_DEFINED
+#include <string>
+#include <iostream>
+#include <pugixml.hpp>
+
+#include <Wt/WApplication>
+
+namespace Utils {
+
+  typedef pugi::xml_node XmlNodeType;
+  const std::string checkFileExists(const std::string& fname);
+
+  const std::string moduleDocRoot();
+  
+  void getValueOfAttribute(const XmlNodeType &node, const std::string attr, std::string& value);
+  void getValueOfAttribute(const XmlNodeType &node, const char* attr, std::string& value);
+
+  void getValueOfChildNode(const XmlNodeType &node, const char* child, std::string& value);
+  void getValueOfChildNode(const XmlNodeType &node, const std::string child, std::string& value);
+
+  void alert( const std::string msg);
+
+}
+
+#endif
